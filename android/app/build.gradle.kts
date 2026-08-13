50308e866522ef01de37c8e1a76b13fcfd009eb8
lladlam <lladlam@outlook.com>
2026-08-13 08:37:22 +0800

    chore: bump Android version to 0.3.0-Dev

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lladlam.melox.android"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.2.0-Dev"
+        versionCode = 3
+        versionName = "0.3.0-Dev"
     }
 
     buildFeatures {
